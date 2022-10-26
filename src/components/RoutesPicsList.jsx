f795ea02d75863be17f053e2e5e562e91c9d91b8
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-10-25 23:31:53 -0500

    before Solana implementation

--- src/components/RoutesPicsList.jsx
+++ src/components/RoutesPicsList.jsx
@@ -21,51 +21,31 @@ export default function RoutesPicsList() {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
+    img: 'https://ilamdir.org/blobs/ilamdir/w300h300-cropped/328/2020/31/convento-santo-domingo.jpg',
+    title: 'Santo Domingo',
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
+    img: 'http://www.guiadeviajesperu.com/wp-content/uploads/2019/12/iglesia-lima.jpg',
+    title: 'San Francisco',
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
+    img: 'https://refrinoticias.com/wp-content/uploads/2015/10/19218687251489261498172492.jpg',
+    title: 'Catedral de Lima',
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
+    img: 'https://freewalkingtoursperu.com/wp-content/uploads/2020/10/iglesia-la-merced-lima-centro-historico.jpg',
+    title: 'La Merced',
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
+    img: 'https://s.ruta0.net/cache/img680/12651.jpg',
+    title: 'Virgen Milagrosa',
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
+    img: 'http://www.guiadeviajesperu.com/wp-content/uploads/2019/12/iglesia-lima-3.jpg',
+    title: 'Las Nazarenas',
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
+    img: 'https://lexlatin.com/sites/default/files/catedral_senora_del_pilar_lima_peru.jpg',
+    title: 'Virgen del Pilar',
   },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-];
+]
